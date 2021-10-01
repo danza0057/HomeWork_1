83810b14d49773ab3eaca36680713daad89a9680
danza0057 <danza0057@yandex.ru>
2021-10-01 12:03:18 +0300

    Initial commit

--- main_1.py
+++ main_1.py
@@ -3,4 +3,4 @@ month = " августа "
 name = input("Введите ваше имя: ")
 age = int(input("Введите ваш возраст: "))
 print("Здравствуйте,", name, "Ваш возраст", age)
-print("Сегодня " + day +month)
+print("Сегодня " + day + month)
--- main_2.py
+++ main_2.py
@@ -1,4 +1,4 @@
-time = int(input("Введите время в секундах: "))
+time = int(input("Введите время в секундах:  "))
 hour = time // 3600
 minute = (time - hour * 3600) // 60
 sec = time % 60
